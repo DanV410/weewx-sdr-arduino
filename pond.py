551adac6dbb2514d5ca786b9315b041413b8ec19
DanV410 <dan.verbus@gmail.com>
2016-09-23 09:19:02 -0400

    Update pond.py
    Added comments

--- pond.py
+++ pond.py
@@ -1,3 +1,38 @@
+#!/usr/bin/python
+#
+# Portions Copyright 2014 Matthew Wall
+# Portions taken from the pond.py example on the "add sensor" page on the weewx wiki, edited by Tom Keffer
+#
+# weewx service that reads data from a file as key value pairs and adds it to every loop packet
+#
+# This driver will read data from a file.  Each line of the file is a 
+# name=value pair, for example:
+#
+# temperature=50
+# humidity=54
+# in_temperature=75
+#
+# The names must match the weewx database schema, but the schema can be extended for new fields
+#
+# The units must be in the weewx.US unit system:
+#   degree_F, inHg, inch, inch_per_hour, mile_per_hour
+#
+# Add this service to weewx.conf, then restart weewx 
+#[Engine]
+#[[Services]]
+#    data_services = ..., user.pond.PondService
+#
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+# See http://www.gnu.org/licenses/
+
 import syslog
 import weewx
 from weewx.wxengine import StdService
